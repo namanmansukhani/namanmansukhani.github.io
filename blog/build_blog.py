a1950bacbe6b0939e982260782f80e3566023ad4
Naman Mansukhani <naman.mansukh@gmail.com>
2022-07-05 20:19:22 +0530

    Added new build instructions for index.html

--- blog/build_blog.py
+++ blog/build_blog.py
@@ -1,12 +1,25 @@
 #!/bin/python3
 
 import os
+import markdown
 
 for file in os.listdir():
     if file.endswith('.md'):
+        if file == 'index.md':
+            continue
         dst = f"{file.replace('.md', '.html')}"
         os.system(f"lowdown {file} > {dst}")
         print(f"converted {file} to {dst}")
+
+with open('index.md') as md:
+    md_text = md.readlines()
+    md_text = ''.join(md_text)
+    html = markdown.markdown(md_text)
+
+with open('index.html', 'w') as f:
+    f.writelines([html])
+
+print('Converted index.md to index.html')
 
 final_cmd = '''../ssg5 . ../blog/ "Naman's Blog" "https://namanmansukhani.github.io/"'''
 
--- blog/index.md
+++ blog/index.md
@@ -1,5 +1,5 @@
 ## Hi! This is my blog
 
-# This is still under work
+#### This is still under work
 
-[Storing your dotfiles on Github] ("https://namanmansukhani.github.io/blog/dotfiles.html")
+- ### [Storing your dotfiles on Github]("https://namanmansukhani.github.io/blog/dotfiles.html")
